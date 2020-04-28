7f459dad690822935556dd402e58bc04d37a03aa
Christophe-Dupin <mail.christophe.dupin@gmail.com>
2020-04-28 13:37:21 +0200

    refacto hero class

--- models/models.py
+++ models/models.py
@@ -1,38 +1,6 @@
 from config import PATH, START, FINISH, MAC
 import random
-
-
-class Position:
-    """Class in order to return position, an update position after move """
-
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __repr__(self):
-        return str((self.x, self.y))
-
-    def __getitem__(self, key):
-        return (self.x, self.y)[key]
-
-    def __eq__(self, pos):
-        return (self.x, self.y) == pos
-
-    def moveUp(self):
-        self.x = self.x - 1
-        return (self.x, self.y)
-
-    def moveDown(self):
-        self.x = self.x + 1
-        return (self.x, self.y)
-
-    def moveLeft(self):
-        self.y = self.y - 1
-        return (self.x, self.y)
-
-    def moveRight(self):
-        self.y = self.y + 1
-        return (self.x, self.y)
+from controller.eventmanager import *
 
 
 class Map:
@@ -45,6 +13,7 @@ class Map:
         self.start = []
         self.finish = []
         self.wall = []
+        self.parse_map()
 
     def __getitem__(self, key):
         return self.liste_map[key]
@@ -73,14 +42,32 @@ class Map:
 class Hero:
     """Storing data for Hero """
 
-    def __init__(self, map):
+    def __init__(self, map, item, x, y):
         self.map = map
-        self.position = self.map.start
+        self.x = x
+        self.y = y
+        self.item = item
 
     def move(self, direction):
-        new_coordonne = getattr(self.position, direction)()
-        if new_coordonne in self.map.path:
-            self.position = new_coordonne
+        if direction == "moveUp":
+            new_coordonne = self.x - 1, self.y
+            if new_coordonne in self.map.path:
+                self.x, self.y = new_coordonne
+
+        if direction == "moveDown":
+            new_coordonne = (self.x + 1, self.y)
+            if new_coordonne in self.map.path:
+                self.x, self.y = new_coordonne
+
+        if direction == "moveLeft":
+            new_coordonne = (self.x, self.y - 1)
+            if new_coordonne in self.map.path:
+                self.x, self.y = new_coordonne
+
+        if direction == "moveRight":
+            new_coordonne = (self.x, self.y + 1)
+            if new_coordonne in self.map.path:
+                self.x, self.y = new_coordonne
 
 
 class Vilain:
@@ -92,11 +79,12 @@ class Vilain:
 
 
 class Item:
+    # Work in Progress
     def __init__(self, map):
         self.map = map
-        self.choosen_item = []
         self.position_items = []
-        self.library = []
+        self.show = True
+        self.random_items_position()
 
     def random_items_position(self):
         self.position_items = random.choices(self.map.path, k=3)
