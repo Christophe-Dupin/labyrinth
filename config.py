4e8f63bac0962287389493fb3315333f0dbc22b4
Christophe-Dupin <mail.christophe.dupin@gmail.com>
2020-04-28 13:34:10 +0200

    add contant Picture and window parameter

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent
+# BASE_DIR = Path(__file__).resolve().parent
 
 # Constant for Terminal
 START = "S"
@@ -10,3 +10,22 @@ PATH = "."
 WALL = "#"
 ITEM = ["<", "^", "!", "§", "(", "%"]
 MAC = "M"
+
+# Constant Pictures
+PATH_PICTURE = "data/ressource/floor.png"
+WALL_PICTURE = "data/ressource/wall.png"
+HERO = "data/ressource/MacGyver.png"
+VILAIN = "data/ressource/Gardien.png"
+BG = "data/ressources/BG.png"
+ITEMS_PICTURES = [
+    "data/ressource/ether.png",
+    "data/ressource/potion.png",
+    "data/ressource/items.png",
+]
+
+# Window Parameter
+NAME_WINDOW = "MACGAVER"
+WIDTH = 300
+HEIGHT = 300
+SPRITE_SIZE = 20
+cote_fenetre = WIDTH * SPRITE_SIZE
