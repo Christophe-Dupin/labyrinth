f34f243753f17ad62b31885568041b7526edfa01
Christophe-Dupin <mail.christophe.dupin@gmail.com>
2020-04-29 21:24:51 +0200

    add victory methode

--- gameManager/gamemanager.py
+++ gameManager/gamemanager.py
@@ -2,29 +2,20 @@ import pygame
 from pygame.locals import *
 from models.models import Map, Item, Hero, Vilain
 from view.view import View
-from config import WIDTH, HEIGHT, SPRITE_SIZE, NAME_WINDOW
+from config import WIDTH, HEIGHT, SPRITE_SIZE, NAME_WINDOW, WIN, LOST
 
 
 class Gamemanager:
-    def __init__(self, map, hero, vilain, item, view):
+    def __init__(self, map, hero, view):
         self.map = map
         self.hero = hero
-        self.vilain = vilain
-        self.item = item
         self.view = view
-        self.generate_map()
-        self.generate_hero()
-
-        # init pygame
-        pygame.init()
-        pygame.display.flip()
-        self.view.window(WIDTH, HEIGHT)
-        self.view.window_title(NAME_WINDOW)
 
     def generate_map(self):
-        self.view.draw_path(self.view.window(WIDTH, HEIGHT), self.map.path, SPRITE_SIZE)
-        self.view.draw_wall(self.view.window(WIDTH, HEIGHT), self.map.wall, SPRITE_SIZE)
-        self.view.draw_vilain(self.view.window(WIDTH, HEIGHT), SPRITE_SIZE)
+        self.view.draw(self.map.path, self.map.wall, self.map.finish)
 
-    def generate_hero(self):
-        self.view.draw_hero(self.view.window(WIDTH, HEIGHT), SPRITE_SIZE)
+    def victory(self):
+        if self.hero.status() == WIN:
+            print("c'est gagné")
+        elif self.hero.status() == LOST:
+            print("c'est perdu")
