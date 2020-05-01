b532672307a4271f6415e2d4fc6b2abb79ae7693
Christophe-Dupin <mail.christophe.dupin@gmail.com>
2020-05-01 17:29:19 +0200

    add getitem and victory methode  add lstItem and number item attribut

--- gameManager/gamemanager.py
+++ gameManager/gamemanager.py
@@ -6,16 +6,26 @@ from config import WIDTH, HEIGHT, SPRITE_SIZE, NAME_WINDOW, WIN, LOST
 
 
 class Gamemanager:
-    def __init__(self, map, hero, view):
+    def __init__(self, map, hero, view, lstItem):
         self.map = map
         self.hero = hero
         self.view = view
+        self.lstItem = lstItem
+        self.number_item = 0
 
     def generate_map(self):
         self.view.draw(self.map.path, self.map.wall, self.map.finish)
 
+    def get_item(self):
+        for i in self.lstItem.item_list:
+            if i[0] == self.hero.x and i[1] == self.hero.y:
+                i.show = False
+                self.number_item += 1
+                print(self.number_item)
+
     def victory(self):
-        if self.hero.status() == WIN:
-            print("c'est gagné")
-        elif self.hero.status() == LOST:
-            print("c'est perdu")
+        for i in self.map.finish:
+            if (self.hero.x, self.hero.y) == i:
+                if self.number_item == len(self.lstItem.position_items):
+                    return WIN
+                return LOST
