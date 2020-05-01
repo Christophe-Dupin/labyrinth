d4be1c3cd2aef7a8b6f3fb45b8ba4ef2a8f44ae9
Christophe-Dupin <mail.christophe.dupin@gmail.com>
2020-05-01 17:31:50 +0200

    remove generate pygame image to sprites classe

--- view/view.py
+++ view/view.py
@@ -1,6 +1,6 @@
 import pygame
 import random
-from models.models import Hero, Map, Vilain, Item
+from models.models import Hero, Map, Vilain, Item, LstItem
 from config import (
     WIDTH,
     HEIGHT,
@@ -8,7 +8,6 @@ from config import (
     PATH_PICTURE,
     WALL_PICTURE,
     SPRITE_SIZE,
-    ITEMS_PICTURES,
     HERO,
     VILAIN,
     FRAMERATE,
@@ -21,54 +20,49 @@ from pygame.locals import *
 class View:
     """ Draws the model state onto the screen."""
 
-    def __init__(self, map, item, hero):
+    def __init__(self, map, hero, lstItem, sprites):
         self.map = map
-        self.item = item
         self.hero = hero
-
-        # viewport size
-        self.screen_size = WIDTH, HEIGHT
-
-        # init pygame
-        pygame.init()
-        pygame.display.set_caption(NAME_WINDOW)
-        self.clock = pygame.time.Clock()
-        self.clock.tick(FRAMERATE)
-        self.screen = pygame.display.set_mode(self.screen_size)
-
-        # convert image
-        self.item_images = [
-            pygame.image.load("data/ressource/ether.png").convert_alpha(),
-            pygame.image.load("data/ressource/potion.png").convert_alpha(),
-            pygame.image.load("data/ressource/items.png").convert_alpha(),
-        ]
-        self.path_image = pygame.image.load(PATH_PICTURE).convert_alpha()
-        self.wall_image = pygame.image.load(WALL_PICTURE).convert_alpha()
-        self.hero_image = pygame.image.load(HERO).convert_alpha()
-        self.vilain_image = pygame.image.load(VILAIN).convert_alpha()
+        self.lstItem = lstItem
+        self.sprites = sprites
 
     def draw(self, path, wall, vilain):
         for path_position in path:
             x, y = path_position
-            self.screen.blit(self.path_image, (y * SPRITE_SIZE, x * SPRITE_SIZE))
+            self.sprites.screen.blit(
+                self.sprites.path_picture, (y * SPRITE_SIZE, x * SPRITE_SIZE)
+            )
 
         for wall_position in wall:
             x, y = wall_position
-            self.screen.blit(self.wall_image, (y * SPRITE_SIZE, x * SPRITE_SIZE))
+            self.sprites.screen.blit(
+                self.sprites.wall_picture, (y * SPRITE_SIZE, x * SPRITE_SIZE)
+            )
 
-            self.screen.blit(
-                self.hero_image,
+            self.sprites.screen.blit(
+                self.sprites.hero_picture,
                 ((self.hero.y * SPRITE_SIZE, self.hero.x * SPRITE_SIZE)),
             )
         for vilain_position in vilain:
             x, y = vilain_position
             if self.hero.status() != WIN:
-                self.screen.blit(self.vilain_image, (y * SPRITE_SIZE, x * SPRITE_SIZE))
+                self.sprites.screen.blit(
+                    self.sprites.vilain_picture, (y * SPRITE_SIZE, x * SPRITE_SIZE)
+                )
 
-        for item_position in self.item.position_item:
-            x, y = item_position
-            if self.item.show:
-                self.screen.blit(self.vilain_image, (y * SPRITE_SIZE, x * SPRITE_SIZE))
+        for i in self.lstItem.item_list:
+            if i.show:
+                self.sprites.screen.blit(
+                    self.sprites.ether_picture, (i[1] * SPRITE_SIZE, i[0] * SPRITE_SIZE)
+                )
+                self.sprites.screen.blit(
+                    self.sprites.potion_picture,
+                    (i[1] * SPRITE_SIZE, i[0] * SPRITE_SIZE),
+                )
+                self.sprites.screen.blit(
+                    self.sprites.potion_picture,
+                    (i[1] * SPRITE_SIZE, i[0] * SPRITE_SIZE),
+                )
 
         pygame.display.flip()
-        self.clock.tick(FRAMERATE)
+        self.sprites.clock.tick(FRAMERATE)
