fa5480de4f02a9f17b69d464d76bac894cc3bd4e
Christophe-Dupin <mail.christophe.dupin@gmail.com>
2020-05-05 10:46:05 +0200

    change window size add new picture

--- config.py
+++ config.py
@@ -16,17 +16,17 @@ PATH_PICTURE = "data/ressource/floor.png"
 WALL_PICTURE = "data/ressource/wall.png"
 HERO = "data/ressource/MacGyver.png"
 VILAIN = "data/ressource/Gardien.png"
-BG = "data/ressources/BG.png"
+BG = "data/ressource/BG.jpg"
 ETHER = "data/ressource/ether.png"
-POTION = "data/ressource/potion.png"
-ITEM = "data/ressource/items.png"
+POTION = "data/ressource/aiguille.png"
+ITEM = "data/ressource/tube_plastique.png"
 
 
 # Window Parameter
 NAME_WINDOW = "MACGAVER"
-WIDTH = 300
-HEIGHT = 320
-SPRITE_SIZE = 20
+WIDTH = 600
+HEIGHT = 640
+SPRITE_SIZE = 40
 FRAMERATE = 60
 # Victory
 IN_MAZE = 0
