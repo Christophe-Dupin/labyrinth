08aecc53cc6d640a2c01204f85d5f68792746e95
Christophe-Dupin <mail.christophe.dupin@gmail.com>
2020-05-11 15:35:52 +0200

    remove gamemanager module

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ add  main loop of the game
 """
 from controller.controller import Controller
 
-from gameManager.gamemanager import Gamemanager
-
-from models.models import Hero, Lstitem, Map, Sprites
+from models.models import Hero, Map, Sprites
 
 from view.view import View
 
@@ -16,21 +14,17 @@ class App:
 
     def __init__(self):
         """Instanciate all the object of the several modules."""
-        self.map = Map("data/map.txt")
-        self.hero = Hero(self.map, 0, 0)
-        self.sprites = Sprites()
-        self.lstitem = Lstitem(self.map, self.sprites)
-        self.view = View(self.map, self.hero, self.lstitem, self.sprites)
-        self.gamemanager = Gamemanager(
-            self.map, self.hero, self.view, self.lstitem
-        )
-        self.controller = Controller(self.gamemanager)
+        self.laby = Map("data/map.txt")
+        self.hero = Hero(0, 0)
+        self.sprites = Sprites(self.laby.path)
+        self.view = View(self.laby, self.hero, self.sprites)
+        self.controller = Controller(self.hero, self.laby, self.sprites, self.view)
 
     def mainloop(self):
         """Loop of the game."""
         while self.controller.running:
             self.controller.process_input()
-            self.gamemanager.generate_map()
+            self.controller.generate_map()
 
 
 if __name__ == "__main__":
